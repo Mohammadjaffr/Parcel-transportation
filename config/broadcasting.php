c198929b4f559f6a6ccfe2f904c56f4ca657f1d1
awad3317 <127610663+awad3317@users.noreply.github.com>
2026-05-10 13:49:43 +0300

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -56,7 +56,7 @@ return [
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
                 'port' => env('PUSHER_PORT', 6001),
                 'scheme' => env('PUSHER_SCHEME', 'http'),
-                'encrypted' => true,
+                'encrypted' => false,
                 'useTLS' => false, 
             ],
         ],
