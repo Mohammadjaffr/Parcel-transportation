8663b5da8b531018907d83cad5a91b0ac6303c82
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2026-05-15 14:13:46 +0400

    UPDATE

--- resources/views/receipts/templates/SenderShipmentReceipt.blade.php
+++ resources/views/receipts/templates/SenderShipmentReceipt.blade.php
@@ -157,12 +157,22 @@
                 </div>
                 <div>
                     <p class="mb-1 text-xs font-bold text-slate-400">المدفوع</p>
-                    <p class="text-lg font-black text-emerald-600" dir="ltr">{{ $partial_amount ?? 0 }} ر.ي</p>
+                    @if ($payment_key == 'customer_credit')
+                        <p class="text-lg font-black text-emerald-600" dir="ltr">{{ $remaining_amount ?? 0 }} ر.ي
+                        </p>
+                    @else
+                        <p class="text-lg font-black text-emerald-600" dir="ltr">{{ $partial_amount ?? 0 }} ر.ي</p>
+                    @endif
+
                 </div>
-                
                 <div>
+                    
                     <p class="mb-1 text-xs font-bold text-slate-400">المتبقي</p>
+                    @if($payment_key == 'customer_credit')
+                    <p class="text-lg font-black text-rose-600" dir="ltr">{{ $total_amount ?? 0 }} ر.ي</p>
+                    @else
                     <p class="text-lg font-black text-rose-600" dir="ltr">{{ $remaining_amount ?? 0 }} ر.ي</p>
+                    @endif
                 </div>
             </div>
         </div>
