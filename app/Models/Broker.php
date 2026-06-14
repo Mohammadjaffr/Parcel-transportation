7f48628eb034286b0ea2cc0cab837b36357ef6de
awad3317 <127610663+awad3317@users.noreply.github.com>
2026-06-14 14:55:00 +0300

    update

--- app/Models/Broker.php
+++ app/Models/Broker.php
@@ -2,13 +2,24 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToApp;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Broker extends Model
 {
-    protected $fillable = ['name'];
+    use BelongsToApp;
 
+    protected $fillable = ['name', 'app_id'];
+
+    protected static function booted(): void
+    {
+        static::creating(function ($model) {
+            if (auth()->check() && auth()->user()->app_id && !$model->app_id) {
+                $model->app_id = auth()->user()->app_id;
+            }
+        });
+    }
     public function passengers(): HasMany
     {
         return $this->hasMany(Passengers::class);
